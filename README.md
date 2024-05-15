271404c0db7dc155ca133082958397abab14b2d7
Nandovga123 <nandovga@hotmail.com>
2024-05-15 19:51:15 -0300

    #Start - Config developer debug

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">@Orangecode/React</h1>
+<p>🚀 Em construção.</p>
---
+++ src/Example.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Example() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
