e8fe5ad3fffaef81e584415a9c2ef61f87f84022
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2024-12-05 15:57:01 -0300

    #9 - Table
    - Ajustar o component de table.
    - Atualização do pacote primereact 10.5.0

--- debug/src/index.tsx
+++ debug/src/index.tsx
@@ -1,75 +1,25 @@
-import React, { useEffect, useRef } from "react";
-
+import React from "react";
 import { Box } from "../../src/box";
-import { TablePivot } from "../../src/tablepivot";
-import { TablePivotProps, TablePivotRefProps } from "../../src/tablepivot/types";
-
-const PivotTableOptions: TablePivotProps["pivotOptions"] = {
-    grid: {
-        showHeaders: false
-    },
-    configuratorButton: false
-};
-
-const PivotTableSlice: TablePivotProps["pivotSlice"] = {
-    rows: [
-        { uniqueName: "prioridade" },
-        { uniqueName: "cliente" },
-    ],
-    measures: [
-        { uniqueName: "valor", active: true, grandTotalCaption: "Total valor" },
-    ],
-    expands: {
-        expandAll: true,
-    }
-};
-
-const PivotTableCustom = (cellBuilder, cellData) => {
-    if (cellData.type == "value") {
-        if (cellData.rowIndex % 2 == 1 && cellData.value > 250) {
-            cellBuilder.text = `<p><img class="me-1" style='width: 20px' src='https://static.wixstatic.com/media/63ea58_3c088f23c07645c6a64aacd2372b431c~mv2.png/v1/fit/w_252,h_336,q_90/63ea58_3c088f23c07645c6a64aacd2372b431c~mv2.png'/>${cellData.value}</p>`;
-        }
-    }
-};
+import { Table } from "../../src/table";
 
 const Root = () => {
-    const pivotRef = useRef<TablePivotRefProps>(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            pivotRef.current?.webdatarocks?.setReport({
-                dataSource: {
-                    data: [
-                        { cliente: "DIRETRIZ INFORMÁTICA", prioridade: "Alta", valor: 50 },
-                        { cliente: "DIRETRIZ INFORMÁTICA", prioridade: "Alta", valor: 200 },
-                        { cliente: "DIRETRIZ INFORMÁTICA", prioridade: "Alta", valor: 106 },
-                        { cliente: "DIRETRIZ INFORMÁTICA", prioridade: "Alta", valor: 100 },
-                        { cliente: "PREF. MUN. ELOI MENDES", prioridade: "Média", valor: 108 },
-                        { cliente: "PREF. MUN. ELOI MENDES", prioridade: "Média", valor: 200 },
-                        { cliente: "PREF. MUN. ELOI MENDES", prioridade: "Baixa", valor: 250 },
-                    ]
-                },
-                options: PivotTableOptions,
-                slice: { ...PivotTableSlice, expands: { expandAll: true } }
-            });
-        }, 1000);
-    });
-
     return (
-        <Box size="100">
-            <Box size="100">
-                <TablePivot toolbarOptions={{
-                    connect: false,
-                    open: false,
-                    fullscreen: false,
-                    save: false,
-                    options: false
-                }}
-                            pivotCustomizeCell={PivotTableCustom}
-                            pivotOptions={PivotTableOptions}
-                            ref={pivotRef}
-                            size="100"/>
-            </Box>
+        <Box size="50">
+            <Table styleResizable
+                   styleStriped
+                   column={[
+                       { id: "id", header: "Cód", style: { userSelect: "text" } },
+                       { id: "name", header: "Nome", style: { userSelect: "text" } },
+                       { id: "age", header: "Idade" },
+                   ]}
+                   data={[
+                       { id: 1, name: "Lara", age: 4 },
+                       { id: 2, name: "Luiz Fernando", age: 30 },
+                       { id: 3, name: "Dayana", age: 28 },
+                       { id: 4, name: "Malu", age: 7 },
+                   ]}
+                   styleSize="small"
+                   styleType="bordered"/>
         </Box>
     );
 };
