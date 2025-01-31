be353ad69adea7d6fababbe13d6f5cada62a6745
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2025-01-31 17:15:39 -0300

    #Upgrade package

--- debug/src/index.tsx
+++ debug/src/index.tsx
@@ -1,37 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "../../src/box";
 import { Table } from "../../src/table";
 
 const Root = () => {
-    const [state, setState] = useState([]);
-
     return (
-        <Box css={{ fontSize: ".9em" }}
-             size="50">
-            <Table paginator
-                   styleStriped
-                   column={[
-                       { id: "id", header: "Código", sort: true },
-                       { id: "name", header: "Nome", sort: true },
-                   ]}
-                   data={[
-                       { id: 1, name: "Nando" },
-                       { id: 2, name: "Giovane" },
-                       { id: 3, name: "Lara" },
-                       { id: 4, name: "Dayana" },
-                       { id: 5, name: "Dayana" },
-                       { id: 6, name: "Dayana" },
-                       { id: 7, name: "Dayana" },
-                       { id: 8, name: "Dayana" },
-                       { id: 9, name: "Dayana" },
-                   ]}
-                   paginatorRow={5}
-                   selection={state}
-                   selectionMode="checkbox"
-                   styleSize="small"
-                   styleType="bordered"
-                   onSelection={setState}/>
-        </Box>
+        <>
+            <Box css={{ fontSize: ".9em" }}
+                 size="50">
+                <Table paginator
+                       styleStriped
+                       column={[
+                           { id: "id", header: "Código", sort: true },
+                           { id: "name", header: "Nome", sort: true },
+                       ]}
+                       data={[
+                           { id: 1, name: "Nando" },
+                           { id: 2, name: "Giovane" },
+                           { id: 3, name: "Lara" },
+                           { id: 4, name: "Dayana" },
+                           { id: 5, name: "Dayana" },
+                           { id: 6, name: "Dayana" },
+                           { id: 7, name: "Dayana" },
+                           { id: 8, name: "Dayana" },
+                           { id: 9, name: "Dayana" },
+                       ]}
+                       paginatorRow={5}
+                       selectionMode="checkbox"
+                       styleSize="small"/>
+            </Box>
+        </>
     );
 };
 export default Root;
