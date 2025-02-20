5da2bd51f6bd82b3406aa6ba6803bf1741fb1612
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2025-02-20 10:44:53 -0300

    ??? - Incluindo lib de test no pacote

--- debug/src/index.tsx
+++ debug/src/index.tsx
@@ -1,30 +1,13 @@
+import React from "react";
 import { Box } from "../../src/box";
-import { Select } from "../../src/select";
-import React, { useEffect, useRef } from "react";
-import { useForm } from "react-hook-form";
 
 const Root = () => {
-    const ref = useRef<any>(null);
-    const { control } = useForm();
-
-    useEffect(() => {
-        console.log(ref);
-    }, []);
-
     return (
         <form id="formLogin">
             <Box className="bg-light rounded"
                  css={{ fontSize: ".95em" }}
                  size="50">
-                <Select init
-                        options={[
-                            { id: 1, name: "Nando" }
-                        ]}
-                        control={control}
-                        label="E-mail"
-                        mode="HookForm"
-                        name="select"
-                        ref={ref}/>
+                <p>Teste</p>
             </Box>
         </form>
     );
