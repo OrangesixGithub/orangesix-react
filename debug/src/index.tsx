161491c73eed9347c7a61b3948260727c7f9663c
Paulo HB Souza <42076064+paulohbsouza@users.noreply.github.com>
2025-02-21 15:19:29 -0300

    Update
    Modificação recurso ordenação componente table. Retorno objeto reordenado para o back-end.

--- debug/src/index.tsx
+++ debug/src/index.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { Box } from "../../src/box";
-import React, { useEffect, useState } from "react";
 import { Table } from "../../src/table";
+import React, { useEffect, useState } from "react";
 
 const Root = () => {
 
@@ -16,11 +15,15 @@ const Root = () => {
 
     useEffect(() => {
         setRegisters([
-            { id: 123, nome: "Paulo Henrique", usuario: "paulo.souza", order: 20 },
-            { id: 231, nome: "Poliana", usuario: "poliana.frogeri", order: 21 },
-            { id: 332, nome: "Yasmin", usuario: "yasmin.souza", order: 22 },
-            { id: 532, nome: "Geovana", usuario: "geovana.souza", order: 23 },
-            { id: 432, nome: "Rafaela", usuario: "rafaela.alves", order: 24 }
+            { id: 111, nome: "Paulo Henrique", usuario: "paulo.souza", order: 20 },
+            { id: 222, nome: "Poliana", usuario: "poliana.frogeri", order: 21 },
+            { id: 333, nome: "Yasmin", usuario: "yasmin.souza", order: 22 },
+            { id: 444, nome: "Geovana", usuario: "geovana.souza", order: 23 },
+            { id: 555, nome: "Pedro", usuario: "pedro.gaspar", order: 24 },
+            { id: 666, nome: "José", usuario: "jose.santos", order: 25 },
+            { id: 777, nome: "Marcos", usuario: "marcos.silva", order: 26 },
+            { id: 888, nome: "Dora", usuario: "dora.alves", order: 27 },
+            { id: 999, nome: "Rafaela", usuario: "rafaela.frogeri", order: 28 }
         ]);
     }, []);
 
@@ -33,7 +36,7 @@ const Root = () => {
                    reorder="all"
                    selection={selectedRow}
                    selectionMode="checkbox"
-                   onReorder={console.log}
+                   onReorder={data => console.log('post: ', data)}
                    onSelection={setSelectedRow}/>
         </Box>
     );
